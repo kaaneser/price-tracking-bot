7bcda93f5ce34587d51dc2e9234ddcceba865f23
kaaneser <kaanyildirimeser@gmail.com>
2025-06-13 00:48:58 +0300

    feature/PTB-2: Implement Auth module for the app - Implemented authentication module using JWT - Implemented a middleware to protect routes - Added login and registration endpoints

--- backend/core/model.js
+++ backend/core/model.js
@@ -5,9 +5,24 @@ const mongoose = require("mongoose");
  * @param {String} name - model name
  * @param {Object} schemaFields - Mongoose schema definition
  * @param {Object} [options] - Optional mongoose schema settings
+ * @param {Object} [methods] - Optional model methods
+ * @param {Object} [preHooks] - Optional schema pre-hooks
  */
-function createModel(name, schemaFields, options = {}) {
+function createModel(
+    name, 
+    schemaFields, 
+    options = {},
+    methods = {},
+    preHooks = {}
+) {
     const schema = new mongoose.Schema(schemaFields, options);
+    
+    for (let hook in preHooks) {
+        schema.pre(hook, preHooks[hook]);
+    }
+
+    schema.methods = { ...schema.methods, ...methods };
+    
     return mongoose.model(name, schema);
 }
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const trackedProductsRouter = require("./trackedProducts");
+const authRouter = require("./auth");
 
 router.use("/tracked-products", trackedProductsRouter);
+router.use("/auth", authRouter);
 
 module.exports = router;
